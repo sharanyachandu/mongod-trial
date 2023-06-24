5a926c876d7f728acde0696bcc94d4bd7fe3298a
sharanya chandu <you@example.com>
2023-06-24 14:35:46 +0530

    auto save

--- docdb.tf
+++ docdb.tf
@@ -1,14 +1,14 @@
-
+/*
 resource "aws_docdb_cluster" "docdb" {
   engine                    = "docdb"
-  cluster_identifier        = "robot-${var.ENV}-docdb"
-  master_username           = "centos"
-  master_password           = "DevOps321"
+  cluster_identifier        =  "robot-${var.ENV}-docdb"
+  master_username           =  "centos"
+  master_password           =  "DevOps321"
   db_subnet_group_name      =  aws_docdb_subnet_group.docdb_subnet_group.name
   skip_final_snapshot       =  true
-  vpc_security_group_ids    = [aws_security_group.allow_mongodb.id]
+  vpc_security_group_ids    =  [aws_security_group.allow_mongodb.id]
 }
-
+*/
 resource "aws_db_subnet_group" "docdb_subnet_group" {
   name       = "robot-${var.ENV}-docdb_subnet_group"
   subnet_ids = data.terraform_remote_state.vpc.outputs.PRIVATE_SUBNET_IDS
@@ -18,8 +18,8 @@ resource "aws_db_subnet_group" "docdb_subnet_group" {
   }
 }
 
-/*
+
 output "output_ref"{
   value = data.terraform_remote_state.vpc
 }
-*/
+
