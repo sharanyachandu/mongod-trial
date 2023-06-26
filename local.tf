873d257aa669f3dd753d575b48ebbf894841cdae
sharanya chandu <you@example.com>
2023-06-26 23:38:27 +0530

    auto save

--- local.tf
+++
@@ -1,4 +0,0 @@
-locals {
-    DOCDB_USER = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_USERNAME"]
-    DOCDB_PASS = jsondecode(data.aws_secretsmanager_secret_version.secrets.secret_string)["DOCDB_PASSWORD"]
-}
